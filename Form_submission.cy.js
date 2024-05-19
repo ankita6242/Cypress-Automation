894a61ba1d665d8345bffe0334ed753736130031
ankita6242 <ankitapawar6242@gmail.com>
2024-05-19 19:04:39 +0530

    Add files via upload

---
+++ Form_submission.cy.js
@@ -0,0 +1,19 @@
+describe('XPathLocators',() =>{
+
+    it('finds list items', () => {
+        cy.visit(" https://docs.google.com/forms/d/e/1FAIpQLScPfEbpaoUu3WVwSDM9wIFX5uo1XQ1xpuHNtP7cF_rkR-o8Zg/viewform")
+        cy.get("//input[@class='whsOnd zHQkBf']").type("Ankita Pawar")
+        cy.get("//div[@id=i12]").click()
+        cy.get("//div[@class='MocG8c HZ3kWc mhLiyf LMgvRb KKjvXb DEh1R']").click()
+        cy.ge("//div[@class='MocG8c HZ3kWc mhLiyf OIC90c LMgvRb']").contains('yes').click()
+        cy.get("//span[@class='NPEfkd RveJvd snByac']").contains('Submit').click()
+        
+
+
+        
+      });
+
+
+
+
+})
